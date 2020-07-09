3ae20a6570c0fbaf6fd9352c784ea48026b2f4a8
huangmiao <huangmiao2468@gmail.com>
2020-07-09 01:51:25 +0100

    added secret key

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
+  # google map API
+  Google_Map_API = Rails.application.secrets.Google_Map_API
+
+
 end
