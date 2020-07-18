8fbdaecda9583e44f7903365e8efa9f0465f3c73
huangmiao <huangmiao2468@gmail.com>
2020-07-18 03:23:13 +0100

    added new routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def logged_in_user
     unless logged_in?
-      flash[:danger] = "Please log in."
+      flash[:danger] = "Please log in first."
       redirect_to login_url
     end
   end
--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,2 +1,23 @@
 class UploadController < ApplicationController
+
+    before_action :logged_in_user
+
+    def loadpage
+    end
+
+    def create
+    end
+
+    def myupload
+    end
+
+    def destroy
+    end
+
+
+    private
+    def load_params
+
+    end
+    
 end
