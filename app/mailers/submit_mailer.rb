9b2c997ff52eb9ed2f00ed52d7a5b8ffdb7b6ad8
huangmiao <huangmiao2468@gmail.com>
2020-07-29 13:06:20 +0100

    corrected mail function

--- app/mailers/submit_mailer.rb
+++ app/mailers/submit_mailer.rb
@@ -1,16 +1,17 @@
 class SubmitMailer < ApplicationMailer
 
 
-    def mail_admin(user_id)
-        mail(to: "huangmiao2468@gmail.com", subject: 'New uploading.')
+    def mail_admin(user_id)      
         current_user = User.find_by(id: user_id)
         @name = current_user.name
         @url = "1234567"
+        mail(to: "huangmiao2468@gmail.com", subject: 'New uploading.')
     end
 
     def mail_user(user_id)
         current_user = User.find_by(id: user_id)
         email = current_user.email
+        @name = current_user.name
         mail(to: email, subject: 'Submitting Recieved.')
     end
 
