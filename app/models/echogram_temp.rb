df88e1c3453445058e8ae01e8aadc8391c2893e7
huangmiao <huangmiao2468@gmail.com>
2020-07-31 13:15:57 +0100

    finame validate

--- app/models/echogram_temp.rb
+++ app/models/echogram_temp.rb
@@ -13,35 +13,82 @@ class EchogramTemp < ApplicationRecord
 
       def gram_valid?
         if gram.attached?
-          if !name_valid?(gram.filename.to_s)
-             errors.add :gram, 'Image file name format error.' 
-          elsif gram.byte_size > (3*1024*1024)  
+          name_valid(gram.filename.to_s)
+          if gram.byte_size > (3*1024*1024)  
             errors.add :gram, 'Image file size is too big.'
-          else
           end
         else
           errors.add :gram, 'You did not choose any image.' 
         end
       end
 
-      def name_valid?(name)
-        temp = name.split(".")
-        format = temp[1]      
+      def name_valid(imagename)
+        if imagename_downcase_valid?(imagename)
+            if imagename_split_valid?(imagename)
+                unless imagename_format_valid?(imagename)
+                  errors.add :gram, 'File format invalid.'
+                end
+                unless imagename_prefix_valid?(imagename) 
+                  errors.add :gram, 'Image file name should start with echogram_.'
+                end
+                unless  imagename_date_valid?(imagename)
+                  errors.add :gram, 'Fishing date in filename is invalid'
+                end
+                unless imagename_time_valid?(imagename)
+                  errors.add :gram, 'Start fishing time in filename is invalid'
+                end
+            else
+              errors.add :gram, 'Filename should be split with _ in right place.'
+            end
+        else
+            errors.add :gram, 'All letters should be in downcase.' 
+        end
+      end
+
+      def imagename_downcase_valid?(name)
         match = true
-        c1 = name[0,9] == "echogram_" 
-        c2 = (1990..2030).cover?(name[9,4].to_i) 
-        c3 = (0..12).cover?(name[13,2].to_i) 
-        c4 = (0..31).cover?(name[15,2].to_i) 
-        c5 = name[17] == "_" 
-        c6 = (0..23).cover?(name[18,2].to_i) 
-        c7 = (0..59).cover?(name[20,2].to_i) 
-        c8 = (0..59).cover?(name[22,2].to_i) 
-        c9 = name[24] == "_" 
-        c10 = ["0038","0120","0200"].include?(name[25,4]) 
-        c11 = name[29] == "." 
-        c12 = ["gif","jpg","jpeg","png"].include?(format)
-        match = c1&&c2&&c3&&c4&&c5&&c6&&c7&&c8&&c9&&c10&&c11&&c12
+        name.each_char do |r|
+          if (65..90).cover?(r.ord)
+            match = false
+          end
+        end
         return match
       end
+
+      def imagename_prefix_valid?(name)        
+        return name[0,9] == "echogram_" 
+      end
+
+      def imagename_date_valid?(name)
+        b1 = (1990..2030).cover?(name[9,4].to_i) 
+        b2 = (0..12).cover?(name[13,2].to_i) 
+        b3 = (0..31).cover?(name[15,2].to_i) 
+        match = b1&&b2&&b3
+        return match
+      end
+
+      def imagename_time_valid?(name)
+        b1 = (0..23).cover?(name[18,2].to_i) 
+        b2 = (0..59).cover?(name[20,2].to_i) 
+        b3 = (0..59).cover?(name[22,2].to_i) 
+        match = b1&&b2&&b3
+        return match
+      end
+
+      def imagename_split_valid?(name)
+        b1 = name[17] == "_" 
+        b2 = name[24] == "_" 
+        b3 = name[29] == "." 
+        match = b1&&b2&&b3
+        return match
+      end
+
+      def imagename_format_valid?(name)
+        temp   = name.split(".")
+        format = temp[1]  
+        match  = ["gif","jpg","jpeg","png"].include?(format)
+        return match
+      end
+
   end
   
